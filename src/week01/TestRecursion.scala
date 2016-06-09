b894ee259438e6a32504b4d23707f8be833f101e
Vincenzo D'Amore <v.damore@gmail.com>
2016-06-09 17:13:24 +0200

    again

--- src/week01/TestRecursion.scala
+++ src/week01/TestRecursion.scala
@@ -10,33 +10,48 @@ object TestRecursion {
          else
            acc.head + sumList(acc.tail)
        }
-       def checkCoin(coins: List[Int], coin: Int, acc: List[Int], counter: Int): Int = {
+       def recursCoin(coins: List[Int], curCoins: Int, counter: Int): Int = {
+          var c = counter
+          if (!coins.isEmpty) {
+            c = checkCoin(coins.tail, coins.head, List(), c)
+            c = recursCoin(coins.tail, coins.head, c);
+          }
+          c
+       }
+       def checkCoin(coins: List[Int], curCoins: Int, acc: List[Int], counter: Int): Int = {
          var c = counter
-         if (!coins.isEmpty) {
+           println(acc)
            var sum = sumList(acc)
            if (sum < money) {
-             c = checkCoin(coins, coin, acc :+ coin, c)
+             println("add " + curCoins)
+             c = checkCoin(coins, curCoins, acc :+ curCoins, c)
            } else if (sum >= money) {
              if (sum == money) {
                c = counter + 1
-               println(acc)
-               print("-")
+               println("- found " + acc)
+//               if (acc.head != curCoins) {
+//                 println("remove " + acc.head)
+//                 c = checkCoin(coins, curCoins, acc.tail, c)
+//               }
              }
-             if (!acc.isEmpty) {
-               if (acc.head != coin)
-                 c = checkCoin(coins, coin, acc.tail, c)
-               else if (!coins.isEmpty)
-                 c = checkCoin(coins.tail, coins.head, acc.tail, c)
+             else {
+               if (!acc.isEmpty) {
+                 if (!coins.isEmpty) {
+                     c = checkCoin(coins.tail, coins.head, acc.tail, c) 
+                 }
+               }
              }
            } 
-         }
          c
        }
-       checkCoin(coins.tail, coins.head, List(), 0)
+       recursCoin(coins, 0, 0)
     }
   
   def main(args: Array[String]) {
-    println(countChange(4,List(1,2,4)))
+    println(countChange(5,List(3,1,2)))
+    println(countChange(6,List(4,2,1)))
+    println(countChange(6,List(1,2,4)))
+    println(countChange(4,List(4,1,2)))
     println(countChange(300,List(500,5,50,100,20,200,10)))
   }
 }
